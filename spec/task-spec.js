7b36461f7af4df849908a8c97b2cf8437fba3956
Dustin Morrill <dmorrill10@gmail.com>
2018-01-02 12:47:04 -0700

    Allow tasks to be marked as done.

--- spec/task-spec.js
+++ spec/task-spec.js
@@ -8,5 +8,15 @@ describe('Task', () => {
     expect(patient.deadline).toBe(null);
     expect(patient.urgency).toBe('neutral');
     expect(patient.notes).toEqual([]);
+    expect(patient.done).toBe(false);
+  });
+  it('can be marked as done with an "x" prefix', () => {
+    var patient = new Task('x Description for task1');
+    expect(patient.name).toBe('');
+    expect(patient.desc).toBe('Description for task1');
+    expect(patient.deadline).toBe(null);
+    expect(patient.urgency).toBe('neutral');
+    expect(patient.notes).toEqual([]);
+    expect(patient.done).toBe(true);
   });
 });
